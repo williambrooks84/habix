a176213d941a4e30c3e388f4fb5fc25d5287ac5a
William Brooks <william.brooks@etu.unilim.fr>
2025-12-03 13:32:59 +0100

    fix

--- src/app/habit/create/page.tsx
+++ src/app/habit/create/page.tsx
@@ -2,6 +2,8 @@ import CreateHabitForm from "@/components/habit/create-form";
 import Categories from '@/components/ui/habit/categories';
 import { getAllCategories } from '@/app/lib/categories';
 import { Button } from '@/components/ui/button';
+import { Suspense } from 'react';
+import Loading from '@/components/ui/loading/loading';
 
 export default async function CreateHabitPage() {
     const categories = await getAllCategories();
@@ -11,7 +13,9 @@ export default async function CreateHabitPage() {
             <h1 className="text-2xl font-semibold text-foreground">Créer une habitude</h1>
 
             <section className="mt-6 mb-22 md:flex md:flex-col md:items-center">
-                <CreateHabitForm categories={categories} />
+                <Suspense fallback={<Loading />}>
+                    <CreateHabitForm categories={categories} />
+                </Suspense>
             </section>
         </div>
     );
